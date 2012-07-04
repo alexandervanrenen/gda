80a2c458423b6b61d4ee649e1c10d1297ce01724
alex-lenovo <alexandervanrenen@googlemail.com>
2012-07-04 19:01:52 +0200

    added clamp function to math

--- include/gda/math.hpp
+++ include/gda/math.hpp
@@ -2,6 +2,7 @@
 #define GDA_MATH_HPP
 //-----------------------------------------------------------------------------
 #include <stdint.h>
+#include <vector>
 //-----------------------------------------------------------------------------
 // Utilities - string processing
 // Alexander van Renen 2011
@@ -15,6 +16,24 @@ template<class T>
 int32_t signum(T t)
 {
    return t>0?1:(t<0?-1:0);
+}
+/// THE CLAMPS !!!
+template<class T>
+void clamp(std::vector<T>& data, T min, T max)
+{
+   for(uint32_t i=0; i<data.size(); i++) {
+      if(data[i]<min)
+         data[i]=min;
+      if(data[i]>max)
+         data[i]=max;
+   }
+}
+/// THE CLAMPS SQUARED !!!
+template<class T>
+void clamp(std::vector<std::vector<T> >& data, T min, T max)
+{
+   for(uint32_t i=0; i<data.size(); i++)
+      clamp(data[i], min, max);
 }
 }
 //-----------------------------------------------------------------------------
